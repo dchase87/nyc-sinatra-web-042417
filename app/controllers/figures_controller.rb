a982052742552a38e92c5595d701cdabcd8fcf3d
mahaffey <ryan.a.mahaffey@gmail.com>
2017-05-18 11:17:34 -0400

    moar

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,15 +1,20 @@
 class FiguresController < ApplicationController
 
-  get '/figures/' do
+  get '/figures' do
+    #binding.pry
+    @figures = Figure.all
     erb :'figures/index'
   end
 
   get '/figures/new' do
     @figures = Figure.all
+    @landmarks = Landmark.all
+    @titles = Title.all
     erb :'figures/new'
   end
 
   post '/figures' do
-
+    @figure = Figure.create(params[:figure])
+    redirect to "/figures/#{@figure.id}"
   end
 end
