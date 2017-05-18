02a4868018773525b2e726b80483c79d55482337
dchase87 <david.chase4@gmail.com>
2017-05-18 11:41:15 -0400

    add new view, create controller action figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,15 +1,24 @@
 class FiguresController < ApplicationController
 
-  get '/figures/' do
+  get '/figures' do
+    @figures = Figure.all
     erb :'figures/index'
   end
 
   get '/figures/new' do
     @figures = Figure.all
+    @landmarks = Landmark.all
+    @titles = Title.all
     erb :'figures/new'
   end
 
   post '/figures' do
+    @figure = Figure.create(params[:figure])
+    redirect "/figures/#{@figure.id}"
+  end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
   end
 end
