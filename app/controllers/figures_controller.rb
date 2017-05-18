ec5ed0fc5871ab868d17a3aff2bf75d790f852c4
mahaffey <ryan.a.mahaffey@gmail.com>
2017-05-18 12:33:15 -0400

    sammy

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -18,6 +18,12 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["name"])
+    end
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["name"])
+    end
     redirect to "/figures/#{@figure.id}"
   end
 
@@ -27,5 +33,15 @@ class FiguresController < ApplicationController
     #binding.pry
     erb :'figures/show'
   end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures/:id/edit' do
+    
+  end
+
 
 end
